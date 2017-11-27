5f9a1235d116286a1a883d51771acb73e75b7562
Hoang Pham T <hoang.pham@asiantech.vn>
2017-11-27 17:25:46 +0700

    Add UI

--- app/routers/router.go
+++ app/routers/router.go
@@ -1,12 +1,12 @@
 package router
 
-import 	"github.com/gorilla/mux"
-import controler "github.com/tsrnd/goweb3/app/controllers"
+import "github.com/gorilla/mux"
+import controler "github.com/goweb3/app/controllers"
 
 func Init() *mux.Router {
 	r := mux.NewRouter()
 	// Serve static files, no directory browsing
-	r.PathPrefix("/assets/").HandlerFunc(controler.Static)	
+	r.PathPrefix("/assets/").HandlerFunc(controler.Static)
 	// r.HandleFunc("/assets/**", controler.Static)
 	r.HandleFunc("/", controler.HelloWorld).Methods("GET")
 	return r
--- main.go
+++ main.go
@@ -1,37 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
-	"encoding/json"
+
 	route "github.com/goweb3/app/routers"
+	"github.com/goweb3/app/shared/database"
 	"github.com/goweb3/app/shared/jsonconfig"
 	"github.com/goweb3/app/shared/view"
-	"github.com/goweb3/app/shared/database"
 )
+
 var config = &configuration{}
 
 // configuration contains the application settings
 type configuration struct {
-	Database database.Info	  `json:"Database"`
-	Template  view.Template   `json:"Template"`
-	View      view.View       `json:"View"`
+	Database database.Info `json:"Database"`
+	Template view.Template `json:"Template"`
+	View     view.View     `json:"View"`
 }
+
 func main() {
 	// Load the configuration file
 	jsonconfig.Load("config"+string(os.PathSeparator)+"config.json", config)
-	database.Connect(config.Database)
+	// database.Connect(config.Database)
 	// Setup the views
 	view.Configure(config.View)
 	view.LoadTemplates(config.Template.Root, config.Template.Children)
-	
+
 	r := route.Init()
-	
+
 	http.ListenAndServe(":8080", r)
-	
+
 }
+
 // ParseJSON unmarshals bytes to structs
 func (c *configuration) ParseJSON(b []byte) error {
 	return json.Unmarshal(b, &c)
 }
-
