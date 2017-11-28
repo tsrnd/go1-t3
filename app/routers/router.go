e86af6ef0654cffd1bba14829d4f7271476e5bcd
Hoang Pham T <hoang.pham@asiantech.vn>
2017-11-28 10:42:20 +0700

    Add UI

--- app/routers/router.go
+++ app/routers/router.go
@@ -9,5 +9,11 @@ func Init() *mux.Router {
 	r.PathPrefix("/assets/").HandlerFunc(controler.Static)
 	// r.HandleFunc("/assets/**", controler.Static)
 	r.HandleFunc("/", controler.HelloWorld).Methods("GET")
+	r.HandleFunc("/login", controler.Login).Methods("GET")
+	r.HandleFunc("/news", controler.News).Methods("GET")
+	r.HandleFunc("/contact", controler.Contact).Methods("GET")
+	r.HandleFunc("/shoe", controler.Login).Methods("GET")
+	r.HandleFunc("/cart", controler.Cart).Methods("GET")
+	r.HandleFunc("/checkout", controler.Checkout).Methods("GET")
 	return r
 }
