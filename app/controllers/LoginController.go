df36312044f34cf0f5c469f6fb498c042c6629a6
at-yenho <yen.ho@asiantech.vn>
2017-11-30 17:31:37 +0700

    show message flash register

--- app/controllers/LoginController.go
+++ app/controllers/LoginController.go
@@ -4,6 +4,7 @@ import "net/http"
 import "github.com/gorilla/csrf"
 import "github.com/goweb3/app/shared/view"
 import service "github.com/goweb3/app/services"
+import "github.com/goweb3/app/shared/flash"	
 
 /**
 *
@@ -13,6 +14,18 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	v := view.New(r)
 	v.Vars[csrf.TemplateTag] = csrf.TemplateField(r)
 	v.Name = "auth/login"
+	emailRequired,_ := flash.GetFlash(w, r, "email-required")
+	emailExist,_ := flash.GetFlash(w, r, "email-exist")
+	nameRequired,_ := flash.GetFlash(w, r, "name-required")
+	passwordRequired,_ := flash.GetFlash(w, r, "password-required")
+	passwordMin,_ := flash.GetFlash(w, r, "password-min")
+	passwordMax,_ := flash.GetFlash(w, r, "password-max")
+	v.Vars["emailRequired"] = string(emailRequired)
+	v.Vars["emailExist"] = string(emailExist)
+	v.Vars["nameRequired"] = string(nameRequired)
+	v.Vars["passwordRequired"] = string(passwordRequired)
+	v.Vars["passwordMin"] = string(passwordMin)
+	v.Vars["passwordMax"] = string(passwordMax)
 	v.Render(w)
 }
 
