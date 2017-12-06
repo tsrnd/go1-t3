cb6a5c174b75a0788c69cc56a53f9e4914552577
at-dungvan <dung.van@asiantech.vn>
2017-12-06 12:02:03 +0700

    login test example and controller structure

--- app/controllers/LoginController.go
+++ app/controllers/LoginController.go
@@ -1,26 +1,34 @@
 package controller
 
-import "net/http"
-import "github.com/gorilla/csrf"
-import "github.com/goweb3/app/shared/view"
-import service "github.com/goweb3/app/services"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/csrf"
+	"github.com/goweb3/app/shared/view"
+
+	service "github.com/goweb3/app/services"
+)
 
 /**
 *
 * Get view Login
 **/
-func Login(w http.ResponseWriter, r *http.Request) {
+func (l *LoginController) Index(w http.ResponseWriter, r *http.Request) {
 	v := view.New(r)
 	v.Vars[csrf.TemplateTag] = csrf.TemplateField(r)
 	v.Name = "auth/login"
-	v.Render(w)
+	l.Render(w, v)
 }
 
 /**
 *
 * Post Login
 **/
-func LoginPost(w http.ResponseWriter, r *http.Request) {
+func (l *LoginController) Login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	email := r.FormValue("email")
+	fmt.Println(email)
 	err := service.Auth(w, r)
 	if err == nil {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -37,3 +45,5 @@ func Logout(w http.ResponseWriter, r *http.Request) {
 	service.Logout(w, r)
 	http.Redirect(w, r, "/", http.StatusFound)
 }
+
+var GetLoginController = &LoginController{Render: renderView}
