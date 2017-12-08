a67710fd3e1f1b05dc200d535b68065a4d9c5230
at-dungvan <dung.van@asiantech.vn>
2017-12-08 16:25:17 +0700

    update template with all image

--- controllers/product.go
+++ controllers/product.go
@@ -22,21 +22,39 @@ func (this *ProductController) Show() {
 	var uint_id = uint(id)
 	product := &models.Product{Id: uint_id}
 	o.Read(product)
-
+	o.LoadRelated(product, "Images")
 	var data = &productData{
-		Name:  product.Name,
-		Id:    product.Id,
-		Price: product.Price,
+		Name:         product.Name,
+		Id:           product.Id,
+		Price:        product.Price,
+		Images:       getMapImage(product.Images...),
+		PrimaryImage: "/static/images/product-details/" + product.Images[0].Image,
 	}
 
 	fmt.Println(data)
+
 	this.Data["product"] = data
 
 	this.TplName = "product/index.html"
 }
 
+func getMapImage(images ...*models.ProductImage) map[int][]string {
+	var a = make(map[int][]string)
+	var mapKey = 0
+	for key, image := range images {
+		if key%3 == 0 {
+			mapKey = key / 3
+		}
+		a[mapKey] = append(a[mapKey], "/static/images/product-details/"+image.Image)
+	}
+	fmt.Println(a)
+	return a
+}
+
 type productData struct {
-	Name  string
-	Id    uint
-	Price int
+	Name         string
+	Id           uint
+	Price        int
+	Images       map[int][]string
+	PrimaryImage string
 }
