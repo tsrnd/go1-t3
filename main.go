47ff37bd1b35619fc1ee5e96a6771c829dd0b313
Dung Van <32283730+at-dungvan@users.noreply.github.com>
2017-12-12 17:09:00 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"encoding/json"
 	"os"
 
-	// "github.com/goweb3/app/models"
-
 	route "github.com/goweb3/app/routers"
 	"github.com/goweb3/app/shared/database"
 	"github.com/goweb3/app/shared/jsonconfig"
